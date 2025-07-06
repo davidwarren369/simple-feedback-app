4333fa091c9a14cd45eed00613b127c8d80618a1
davidwarren369 <davidwarren369@gmail.com>
2025-07-06 21:53:07 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <div>
+        <h1>Feedback App</h1>
+        <Form/>
+      </div>
+      
+    </>
+  )
+}
+
+export default App
